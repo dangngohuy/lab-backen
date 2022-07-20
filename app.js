268bed65cd600bf158c3543fa2d14b3b61ff8cbe
dangngohuy <109237240+dangngohuy@users.noreply.github.com>
2022-07-20 21:04:16 +0700

    cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./routes/contact.route")
+const ApiError =require("./api-error");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "Xin chao den ung dung Quan ly danh ba!"});
+});
+
+app.use("/api/contacts", contactsRouter)
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
+
+module.exports = app;
+
+
+
---
+++ config/index.js
@@ -0,0 +1,11 @@
+const config = {
+    app: {
+        port: 3000,
+    },
+
+    db: {
+        uri: "mongodb://127.0.0.1:27017/Contactbook-backe",
+    }
+};
+
+module.exports = config;
